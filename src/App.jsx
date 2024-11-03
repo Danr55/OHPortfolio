b7fd92ecc5711f9edfaa617e87dfc010ac38e3ad
Daniel Rendon <drendon@Daniels-MacBook-Pro.local>
2024-11-02 22:56:50 -0700

    Added bootstrap styling and some CSS to navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '../src/assets/styles.css'
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import About from '../src/pages/About.jsx'
@@ -5,18 +8,21 @@ import Portfolio from '../src/pages/Portfolio.jsx'
 import Contact from '../src/pages/Contact.jsx'
 import Header from '../src/components/Header'
 import Footer from '../src/components/Footer'
-
+import Resume from '../src/pages/Resume.jsx'
 
 function App() {
   return (
     <Router>
     <div>
       <Header />
+      <div className="container custom-bg3">
       <Routes>
         <Route path="/" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
         <Route path="/contact" element={<Contact />} />
-      </Routes>  
+      </Routes>
+      </div>  
       <Footer />
     </div>
     </Router>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 const Header = () => {
     console.log("Header rendered")
     return (
         <header>
-        <nav>
-            <h1>Oscar Rendon</h1>
-            <ul>
-            <li>
-                <Link to="/">About</Link>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light custom-bg1">
+            <div className="container-fluid">
+            <h1 className="navbar-brand col-2 text-white">Oscar Rendon</h1>
+            <button className="navbar-toggle custom-bg2 rounded-pill d-lg-none" type="button" data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse text-center" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+                <Link to="/" className="nav-link active text-white" aria-current="page">About</Link>
             </li>
-            <li>
-                <Link to="/portfolio">Portfolio</Link>
+            <li className="nav-item">
+                <Link to="/portfolio" className="nav-link text-white">Portfolio</Link>
             </li>
-            <li>
-                <Link to="/contact">Contact</Link>
+            <li className="nav-item">
+                <Link to="/Resume" className="nav-link text-white">Resume</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/contact" className="nav-link text-white">Contact</Link>
             </li>
             </ul>
+            </div>
+            </div>
         </nav>
         </header>
     );
