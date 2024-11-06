7f53412afe40c9a63d75b1b74e99dad80b10497d
Daniel Rendon <drendon@Daniels-MacBook-Pro.local>
2024-11-05 19:35:26 -0800

    Added better background vh

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,11 +5,13 @@ const About = () => {
         <div>
             <h2 className='fs-2 bordered-text'>About Me</h2>
             <img src='./src/assets/man_14.png' alt='oscar rendon' className='img-fluid img-sm' style={{ width: '50px', height: '50px'}}/>
-            <p className='fs-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                 Similique, cum nostrum? Quis quisquam, nisi dolore, 
-                 recusandae mollitia cupiditate consequuntur voluptatem 
-                 aperiam aliquid adipisci natus suscipit velit dolores 
-                 explicabo maxime alias.
+            <p className='fs-5'>Passionate front-end developer with a solid foundation in HTML, 
+                CSS, and JavaScript, and a strong focus on creating responsive, dynamic web applications. 
+                Skilled in React and TypeScript, I bring robust type safety and modern 
+                front-end architecture to my projects. With experience in PostgreSQL, 
+                I also ensure efficient data management and seamless integration with back-end systems. 
+                Constantly expanding my skills, I enjoy building user-centered applications that 
+                combine clean code with engaging design.
             </p>
         </div>
     );
