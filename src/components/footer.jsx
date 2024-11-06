45b22db474fc4fb303b02b03df892e22bda2cc9a
Daniel Rendon <drendon@Daniels-MacBook-Pro.local>
2024-11-05 22:27:01 -0800

    deploy fixes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,15 +5,15 @@ function Footer() {
         <footer className="fixed-bottom custom-bg1 text-center h-20">
         <div className="container">
         <a href="https://github.com/Danr55" target="_blank" rel="noopener noreferrer" className="mx-2">
-            <img src="./src/assets/github_3291695.png" alt="GitHub" className="img-fluid"
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Github_logo_svg.svg/1200px-Github_logo_svg.svg.png?20230420150203" alt="GitHub" className="img-fluid"
             style={{ width: "30px", height: "30px"}}/>
         </a>
         <a href="https://www.linkedin.com/in/oscar-rendon-070b49326/" target="_blank" rel="noopener noreferrer" className="mx-2">
-            <img src="./src/assets/linkedin_2504923.png" alt="LinkedIn" className="img-fluid"
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png?20140125013055" alt="LinkedIn" className="img-fluid"
             style={{ width: "30px", height: "30px"}}/>
         </a>
         <a href="https://x.com/danr1255" target="_blank" rel="noopener noreferrer" className="mx-2">
-        <img src="./src/assets/Twitter.png" alt="Twitter" className="img-fluid"
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/X_logo_2023.svg/640px-X_logo_2023.svg.png" alt="Twitter" className="img-fluid"
             style={{ width: "30px", height: "30px"}}/>
         </a>
         </div>
